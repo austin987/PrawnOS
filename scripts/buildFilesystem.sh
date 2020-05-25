b17c7be9fa225e504596cc7268644ce7afccd2ac
Austin English <austinenglish@gmail.com>
2020-05-25 02:29:10 -0500

    blah

--- scripts/buildFilesystem.sh
+++ scripts/buildFilesystem.sh
@@ -50,7 +50,7 @@ KVER=$1
 DEBIAN_SUITE=$2
 BASE=$3
 
-outmnt=$(mktemp -d -p `pwd`)
+outmnt="$(mktemp -d -p "$(pwd)")"
 
 outdev=/dev/loop5
 
@@ -62,15 +62,15 @@ build_resources=resources/BuildResources
 cleanup() {
   set +e
 
-  umount -l $outmnt > /dev/null 2>&1
-  rmdir $outmnt > /dev/null 2>&1
-  losetup -d $outdev > /dev/null 2>&1
+  umount -l "$outmnt" > /dev/null 2>&1
+  rmdir "$outmnt" > /dev/null 2>&1
+  losetup -d "$outdev" > /dev/null 2>&1
 
   set +e
 
-  umount -l $outmnt > /dev/null 2>&1
-  rmdir $outmnt > /dev/null 2>&1
-  losetup -d $outdev > /dev/null 2>&1
+  umount -l "$outmnt" > /dev/null 2>&1
+  rmdir "$outmnt" > /dev/null 2>&1
+  losetup -d "$outdev" > /dev/null 2>&1
 }
 
 trap cleanup INT TERM EXIT
@@ -137,7 +137,7 @@ build_install_crossystem() {
 }
 
 # create a 2GB image with the Chrome OS partition layout
-create_image $BASE $outdev 50M 40 $outmnt
+create_image $BASE "$outdev" 50M 40 $outmnt
 
 # use default debootstrap mirror if none is specified
 if [ "$PRAWNOS_DEBOOTSTRAP_MIRROR" = "" ]
@@ -147,8 +147,8 @@ fi
 
 # install Debian on it
 export DEBIAN_FRONTEND=noninteractive
-qemu-debootstrap --arch armhf $DEBIAN_SUITE --include locales,init --keyring=$build_resources/debian-archive-keyring.gpg $outmnt $PRAWNOS_DEBOOTSTRAP_MIRROR
-chroot $outmnt passwd -d root
+qemu-debootstrap --arch armhf $DEBIAN_SUITE --include locales,init --keyring=$build_resources/debian-archive-keyring.gpg "$outmnt" $PRAWNOS_DEBOOTSTRAP_MIRROR
+chroot "$outmnt" passwd -d root
 
 
 #Place the config files and installer script and give them the proper permissions
@@ -164,75 +164,76 @@ chmod +x $outmnt/*.sh
 
 #Setup the chroot for apt
 #This is what https://wiki.debian.org/EmDebian/CrossDebootstrap suggests
-cp /etc/hosts $outmnt/etc/
-cp $build_resources/sources.list $outmnt/etc/apt/sources.list
-sed -i -e "s/suite/$DEBIAN_SUITE/g" $outmnt/etc/apt/sources.list
+cp /etc/hosts "$outmnt/etc/"
+cp "$build_resources/sources.list" "$outmnt/etc/apt/sources.list"
+sed -i -e "s/suite/$DEBIAN_SUITE/g" "$outmnt/etc/apt/sources.list"
 if [ "$DEBIAN_SUITE" != "sid" ]
 then
     # sid doesn't have updates or security; they're present for all other suites
-    cat $build_resources/updates.list >> $outmnt/etc/apt/sources.list
-    sed -i -e "s/suite/$DEBIAN_SUITE/g" $outmnt/etc/apt/sources.list
+    cat "$build_resources/updates.list" >> "$outmnt/etc/apt/sources.list"
+    sed -i -e "s/suite/$DEBIAN_SUITE/g" "$outmnt/etc/apt/sources.list"
     # sid doesn't have backports; it's present for all other suites
-    cp $build_resources/backports.list $outmnt/etc/apt/sources.list.d/
-    sed -i -e "s/suite/$DEBIAN_SUITE/g" $outmnt/etc/apt/sources.list.d/backports.list
+    cp "$build_resources/backports.list" "$outmnt/etc/apt/sources.list.d/"
+    sed -i -e "s/suite/$DEBIAN_SUITE/g" "$outmnt/etc/apt/sources.list.d/backports.list"
     #setup apt pinning
-    cp $build_resources/backports.pref $outmnt/etc/apt/preferences.d/
-    sed -i -e "s/suite/$DEBIAN_SUITE/g" $outmnt/etc/apt/preferences.d/backports.pref
+    cp "$build_resources/backports.pref" "$outmnt/etc/apt/preferences.d/"
+    sed -i -e "s/suite/$DEBIAN_SUITE/g" "$outmnt/etc/apt/preferences.d/backports.pref"
     # Install sid (unstable) as an additional source for bleeding edge packages.
-    cp $build_resources/sid.list $outmnt/etc/apt/sources.list.d/
+    cp "$build_resources/sid.list" "$outmnt/etc/apt/sources.list.d/"
     #setup apt pinning
-    cp $build_resources/sid.pref $outmnt/etc/apt/preferences.d/
+    cp "$build_resources/sid.pref" "$outmnt/etc/apt/preferences.d/"
 fi
 if [ "$DEBIAN_SUITE" = "buster" ]
 then
     # Install bullseye (testing) as an additional source
-    cp $build_resources/bullseye.list $outmnt/etc/apt/sources.list.d/
+    cp "$build_resources/bullseye.list" "$outmnt/etc/apt/sources.list.d/"
     #setup apt pinning
-    cp $build_resources/bullseye.pref $outmnt/etc/apt/preferences.d/
+    cp "$build_resources/bullseye.pref" "$outmnt/etc/apt/preferences.d/"
 fi
 
 #Setup the locale
-cp $build_resources/locale.gen $outmnt/etc/locale.gen
-chroot $outmnt locale-gen
+cp "$build_resources/locale.gen" "$outmnt/etc/locale.gen"
+chroot "$outmnt" locale-gen
 
 #Install the base packages
-chroot $outmnt apt update
-chroot $outmnt apt install -y udev kmod net-tools inetutils-ping traceroute iproute2 isc-dhcp-client wpasupplicant iw alsa-utils cgpt vim-tiny less psmisc netcat-openbsd ca-certificates bzip2 xz-utils ifupdown nano apt-utils git kpartx gdisk parted rsync busybox-static cryptsetup bash-completion libnss-systemd libpam-cap nftables uuid-runtime libgpg-error-l10n libatm1 laptop-detect e2fsprogs-l10n vim
+chroot "$outmnt" apt update
+chroot "$outmnt" apt install -y udev kmod net-tools inetutils-ping traceroute iproute2 isc-dhcp-client wpasupplicant iw alsa-utils cgpt vim-tiny less psmisc netcat-openbsd ca-certificates bzip2 xz-utils ifupdown nano apt-utils git kpartx gdisk parted rsync busybox-static cryptsetup bash-completion libnss-systemd libpam-cap nftables uuid-runtime libgpg-error-l10n libatm1 laptop-detect e2fsprogs-l10n vim
 
 #build and install crossystem/mosys, funky way to call the bash function inside the chroot
 export -f build_install_crossystem
-chroot $outmnt /bin/bash -ec "build_install_crossystem"
+chroot "$outmnt" /bin/bash -ec "build_install_crossystem"
 
 #add the live-boot fstab
-cp -f $build_resources/external_fstab $outmnt/etc/fstab
-chmod 644 $outmnt/etc/fstab
+cp -f "$build_resources/external_fstab" "$outmnt/etc/fstab"
+chmod 644 "$outmnt/etc/fstab"
 
 #Cleanup to reduce install size
-chroot $outmnt apt-get autoremove --purge
-chroot $outmnt apt-get clean
+chroot "$outmnt" apt-get autoremove --purge
+chroot "$outmnt" apt-get clean
 
 #Download support for libinput-gestures
 #Package is copied into /InstallResources/packages
-chroot $outmnt apt install -y libinput-tools xdotool
+chroot "$outmnt" apt install -y libinput-tools xdotool
 
 # target unstable for now to work around #173, without breaking libinput-gestures
 #TODO: when package build system is available, targeting unstable won't be necessary.
-chroot $outmnt apt install -y -t unstable build-essential
+chroot "$outmnt" apt install -y -t unstable build-essential
 
-chroot $outmnt apt-get install -y -t unstable -d xsecurelock
+chroot "$outmnt" apt-get install -y -t unstable -d xsecurelock
 
 #Download the packages to be installed by scripts/InstallScripts/InstallPackages.sh:
+# shellcheck source=packages/dependencies
 source "$(dirname "${0}"/../packages/dependencies)"
-chroot $outmnt apt-get install -y -d "$packages_to_cache"
+chroot "$outmnt" apt-get install -y -d "$packages_to_cache"
 
 # grab chromium as well, since sound is still broken in firefox for some media
 
 #Cleanup hosts
-rm -rf $outmnt/etc/hosts #This is what https://wiki.debian.org/EmDebian/CrossDebootstrap suggests
-echo -n "127.0.0.1        PrawnOS" > $outmnt/etc/hosts
+rm -rf "$outmnt/etc/hosts" #This is what https://wiki.debian.org/EmDebian/CrossDebootstrap suggests
+echo -n "127.0.0.1        PrawnOS" > "$outmnt/etc/hosts"
 
-umount -l $outmnt > /dev/null 2>&1
-rmdir $outmnt > /dev/null 2>&1
-losetup -d $outdev > /dev/null 2>&1
+umount -l "$outmnt" > /dev/null 2>&1
+rmdir "$outmnt" > /dev/null 2>&1
+losetup -d "$outdev" > /dev/null 2>&1
 echo "DONE!"
 trap - INT TERM EXIT
